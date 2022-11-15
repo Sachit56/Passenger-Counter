b1d0a873e0464b80408923a9104908ac5bca9dee
Sachit56 <sachitkhadka73@gmail.com>
2022-11-15 21:57:11 +0545

    Done

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-//document.getElementById("count").innerText=4;
+// document.getElementById("count").innerText=4;
 
-function increment() {
-   console.log("Clicked");
-}
+ let countel=document.getElementById("count-el")
+ let saveel=document.getElementById("previous")
+
+ console.log(countel);
+
+ let count=0;
+
+
+ function increment() {
+    count+=1;
+    countel.textContent=count;
+   
+ }
+
+ function save(){
+
+    let countstr=count+" - "
+    saveel.textContent+=countstr;
+    countel.textContent=0
+        count=0;
+
+ }
